53da1428f0dd5ab6d85567030fc27c4eea497ff2
Shalini-jha-dev <74014550+Shalini-jha-dev@users.noreply.github.com>
2022-01-24 18:36:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("poseNet is initialised");
+}
+
+function gotPoses(results) {
+    if (
+        results.length > 0
+    ) {
+        console.log(results);
+    }
+}
